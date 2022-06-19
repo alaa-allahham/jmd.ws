cfaf82f52ef54897dae4b3f9f5996d1785a2b907
alaa-allahham <alaa.allahham@gmail.com>
2022-06-19 14:56:26 +0300

    first setup

---
+++ help.php
@@ -0,0 +1,236 @@
+<?php
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+	<link rel="stylesheet" href="styles.css">
+	<title>JMD WS System</title>
+</head>
+<body>
+
+	<header>
+		
+		<nav class="navigation">
+			<a href="#services">الخدمات</a>
+			<a href="#projects">الأنظمة</a>
+			<a href="#contact">الدعم الفني</a>
+		</nav>
+    <a href="index.php" class="logo">نظام محطات الرصد الجوي</a>
+
+	</header>
+
+	<section class="main">
+
+		<div>
+
+			<h2><span class = "welcome">اهلا و سهلا بك في</span><br><span class="system">نظام محطات الرصد الجوي</span></h2>
+			<h3> نظام يوفر واجهة برمجية لجميع إحتياجات العمل للراصد الجوي</h3>
+			<a href="#projects" class="main-btn">تعرف على الأنظمة</a>
+			<div class="social-icons">
+				<a href="http://jmd.gov.jo/"><img src="images/jmd2.png" alt="jmd"></a>
+				<a href="https://www.youtube.com/channel/UC53UJX8r90IIlHjoXzcQ0Pw"><i class="fa-brands fa-youtube"></i></a>
+				<a href="https://web.facebook.com/jometeo"><i class="fab fa-facebook"></i></a>
+				<a href="https://www.instagram.com/jometeo"><i class="fab fa-instagram-square"></i></a>
+			</div>
+		</div>
+	</section>
+
+	<section class="cards" id="services">
+		<h2 class="title">الخدمات</h2>
+		<div class="content">
+			<div class=card>
+				<div class="icon">
+					<img src="images/ws.png" alt="jmd">
+				</div>
+				<div class="info">
+					<h3>محطة الرصد</h3>
+					<p>عبارة عن نافذة توفر للراصد الجوي في محطة الرصد
+          ما يلزمة من أنظمة للقيام بمهام عملة اليومي و هي: نظام لإرسال النشرات الجوية إلكترونيا على مدار الساعة , و نظام للحضور و الإنصراف إلكتروني لإثبات دوام الراصد, بالاضافة الى نظام المسير نت لإرسال النشرات الخاصة من خلالة
+          </p>
+
+				</div>
+
+			</div>
+			<div class=card>
+				<div class="icon">
+					<img src="images/ts.png" alt="jmd">
+				</div>
+				<div class="info">
+					<h3>طلبات الصيانة</h3>
+					<p>
+            عبارة عن نافذة توفر للراصد الجوي نظامين لتقديم طلبات الصيانة إلكترونيا: الأول خاص بمشاكل الإنترنت و أجهزة الكمبيوتر و غيرها المتعلقة بتكنولوحيا المعلومات يقدم الى شعبة الدعم الفني لموظفي الأرصاد الجوية, أما النظام الثاني فخاص بجميع المشاكل المتعلقة بأجهزة الرصد و الصيانة العامة يقدم لقسم الأجهزة و الصيانة
+          </p>
+
+				</div>
+
+			</div>
+			<div class=card>
+				<div class="icon">
+					<img src="images/employee.png" alt="jmd">
+				</div>
+				<div class="info">
+					<h3>الموظف</h3>
+					<p>
+            عبارة عن نافذة توفر للراصد الجوي الأنظمة التي تلزمة لارسال و استلام معاملاته الرسمية في العمل سواء في إدارة الأرصاد الجوية أو وزارة النقل من خلال نظام البريد الالكتروني الحكومي بالاضافة لنظام 
+            GRP
+          و علاوة على ذلك توفر رابط يمكن الموظف من الإطلاع على بياناته الرسمية في ديوان الخدمة المدنية
+          </p>
+
+				</div>
+
+			</div>
+
+      <div class=card>
+				<div class="icon">
+					<img src="images/logo2.png" alt="jmd">
+				</div>
+				<div class="info">
+					<h3>الأرصاد الجوية</h3>
+					<p>
+             عبارة عن نافذة توفر للراصد الجوي جميع روابط أنظمة و مواقع إدارة الأرصاد الجوية و هي: الموقع الإلكتروني, صفحة الأرصاد على موقع فيسبوك, حساب الأرصاد على موقع الإنستجرام, قناة الأرصاد على موقع اليوتيوب , بالإضافة الى موقع رادار الطقس الجوي التابع للأرصاد الجوية
+          </p>
+
+				</div>
+
+			</div>
+
+		</div>
+
+	</section>
+
+	<section class="projects" id="projects">
+		<h2 class="title">الأنظمة</h2>
+		<div class="content">
+			<div class="project-card">
+				<div class="project-images">
+					<img src="images/Bulletin.png">
+				</div>
+				<div class="project-info">
+					<p class="project-category">نظام إرسال النشرة الجوية</p>
+					<strong class="project-title">
+						<a href="#" class="more-details">مزيد من المعلومات</a>
+            <span>الدعم الفني</span>
+					</strong>
+				</div>
+			</div>
+			<div class="project-card">
+				<div class="project-images">
+					<img src="images/attendance.png">
+				</div>
+				<div class="project-info">
+					<p class="project-category">نظام الحضور و الإنصراف</p>
+					<strong class="project-title">
+						<a href="#" class="more-details">مزيد من المعلومات</a>
+            <span>الدعم الفني</span>
+					</strong>
+				</div>
+			</div>
+			<div class="project-card">
+				<div class="project-images">
+					<img src="images/messir.png">
+				</div>
+				<div class="project-info">
+					<p class="project-category">نظام المسير نت</p>
+					<strong class="project-title">
+						<a href="#" class="more-details">مزيد من المعلومات</a>
+            <span>نظم الإتصالات</span>
+					</strong>
+				</div>
+			</div>
+			<div class="project-card">
+				<div class="project-images">
+					<img src="images/ts2.png">
+				</div>
+				<div class="project-info">
+					<p class="project-category">نظام طلبات الصيانة</p>
+					<strong class="project-title">
+						<a href="#" class="more-details">مزيد من المعلومات</a>
+            <span>الدعم الفني / الأجهزة</span>
+					</strong>
+				</div>
+			</div>
+			<div class="project-card">
+				<div class="project-images">
+					<img src="images/email.png">
+				</div>
+				<div class="project-info">
+					<p class="project-category">البريد الإلكتروني الحكومي</p>
+					<strong class="project-title">
+						<a href="#" class="more-details">مزيد من المعلومات</a>
+            <span>الدعم الفني</span>
+					</strong>
+				</div>
+			</div>
+			<div class="project-card">
+				<div class="project-images">
+					<img src="images/grp.png">
+				</div>
+				<div class="project-info">
+					<p class="project-category">GRP System</p>
+					<strong class="project-title">
+						<a href="#" class="more-details">مزيد من المعلومات</a>
+            <span>الدعم الفني / وزارة النقل</span>
+					</strong>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section class="cards contact" id="contact">
+		<h2 class="title">في حالة واجهتك أي مشكلة بالنظام</h2>
+		<div class="content">
+			<div class=card>
+				<div class="icon">
+					<i class="fas fa-phone"></i>
+				</div>
+				<div class="info">
+					<h3>Phone</h3>
+					<p>+962799846069</p>
+
+				</div>
+
+			</div>
+			<div class=card>
+				<div class="icon">
+					<i class="fas fa-envelope"></i>
+				</div>
+				<div class="info">
+					<h3>Email</h3>
+					<p>alaa.allahham@mot.gov.jo</p>
+
+				</div>
+
+			</div>
+
+
+		</div>
+
+
+	</section>
+
+	<footer class="footer">
+		<p class="footer-title">Copyrights @<span>Alaa Allahham</span></p>
+		<div class="social-icons">
+				<a href="https://www.linkedin.com/in/alaa-allahham-950b7016/"><i class="fab fa-linkedin"></i></a>
+				<a href="https://twitter.com/alaaallahham81"><i class="fab fa-twitter"></i></a>
+				<a href="https://www.facebook.com/alaa.allahham/"><i class="fab fa-facebook"></i></a>
+				<a href="https://www.instagram.com/alaa.allahham/?hl=en"><i class="fab fa-instagram-square"></i></a>
+			</div>
+
+
+
+	</footer>
+
+
+
+
+
+
+</body>
+</html>
